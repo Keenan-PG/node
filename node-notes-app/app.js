279e06f168b8d6eb0cef4d915591e16fe7051aec
Keenan-PG.Keenan <kgolding8991@hotmail.com>
2019-05-09 13:44:04 +0100

    chalk challenge - 4

--- node-notes-app/app.js
+++ node-notes-app/app.js
@@ -1,8 +1,14 @@
 const validator = require('validator');
+const ch = require('chalk');
 const getNotesFunc = require('./notes.js');
 
+const log = console.log;
 const notes = getNotesFunc();
 const email = "keenan@golding.com"
 
-console.log(notes);
-console.log(validator.isEmail(email));
+log(notes);
+log(validator.isEmail(email));
+
+// chalk challenge
+
+log(ch.green.inverse.bold('Success!'));

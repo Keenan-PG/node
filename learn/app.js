e4a6d58fba531569ca6437fdd42250656060c316
Golding <keenan.golding@accenture.com>
2019-08-12 16:31:43 +0100

    express

--- learn/app.js
+++ learn/app.js
@@ -1,13 +1,14 @@
+// local module(s)
 const { sum } = require('./exports.js');
+// npm module(s)
+const exp = require('express');
+const app = exp();
+// shortcut(s)
 const log = console.log;
 
+app.get('/', (req, res) => {
+    res.send("hello world from express")
+}).listen(3200);
+
+
 log(sum(2, 4));
-
-// node modules
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end("hello world :/");
-})
-
-server.listen(3200);
